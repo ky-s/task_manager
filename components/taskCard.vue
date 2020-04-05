9d8fc1ff7abe21aca3afba5cf695b3f909005e15
ky-s <kyohei_86@hotmail.com>
2020-04-05 22:57:19 +0900

    create task_manager app

---
+++ components/taskCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      {{ name }}
+    </v-card-title>
+    <v-card-text>
+      {{ description }}
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn
+        color="primary"
+        nuxt
+        to="/"
+        target="_blank"
+      >
+        Link
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    description: String
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+      </div>
+
+      <v-row>
+        <v-col cols="12" sm="4" v-for="task in tasks" v-bind:key="task.name">
+          <task-card :name="task.name" :description="task.description" />
+        </v-col>
+      </v-row>
+
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import TaskCard from '~/components/taskCard.vue'
+
+export default {
+  components: {
+    TaskCard
+  },
+  data() {
+    var tasks = []
+
+    for (var i = 0; i < 100; i++) {
+      tasks = tasks.concat([
+        { name: 'test' + i,  description: 'test を test が test で test に test する' }
+      ])
+    }
+    return { tasks }
+  }
+}
+</script>
