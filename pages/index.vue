8bc4413833504fe4d1e40e9f47623d02917d4b37
ky-s <kyohei_86@hotmail.com>
2020-04-05 23:14:15 +0900

    [update] add vuedraggable

--- pages/index.vue
+++ pages/index.vue
@@ -13,9 +13,18 @@
       </div>
 
       <v-row>
-        <v-col cols="12" sm="4" v-for="task in tasks" v-bind:key="task.name">
-          <task-card :name="task.name" :description="task.description" />
-        </v-col>
+        <draggable tag="v-col" cols="12" sm="4" :options="{group:'ITEMS'}">
+          <h1>TODO</h1>
+          <task-card v-for="task in todos" v-bind:key="task.name" :name="task.name" :description="task.description" />
+        </draggable>
+        <draggable tag="v-col" cols="12" sm="4" :options="{group:'ITEMS'}">
+          <h1>In progress</h1>
+          <task-card v-for="task in inProgresses" v-bind:key="task.name" :name="task.name" :description="task.description" />
+        </draggable>
+        <draggable tag="v-col" cols="12" sm="4" :options="{group:'ITEMS'}">
+          <h1>Done</h1>
+          <task-card v-for="task in dones" v-bind:key="task.name" :name="task.name" :description="task.description" />
+        </draggable>
       </v-row>
 
     </v-flex>
@@ -23,21 +32,42 @@
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 import TaskCard from '~/components/taskCard.vue'
 
 export default {
   components: {
+    draggable,
     TaskCard
   },
   data() {
     var tasks = []
 
-    for (var i = 0; i < 100; i++) {
-      tasks = tasks.concat([
+    var tasks = {
+      todos: [],
+      inProgresses: [],
+      dones: []
+    }
+
+    for (let i = 1; i <= 20; i++) {
+      tasks.todos = tasks.todos.concat([
         { name: 'test' + i,  description: 'test を test が test で test に test する' }
       ])
     }
-    return { tasks }
+
+    for (let i = 1; i <= 20; i++) {
+      tasks.inProgresses = tasks.inProgresses.concat([
+        { name: 'test' + (i + 20),  description: 'test を test が test で test に test する' }
+      ])
+    }
+
+    for (let i = 1; i <= 20; i++) {
+      tasks.dones = tasks.dones.concat([
+        { name: 'test' + (i + 40),  description: 'test を test が test で test に test する' }
+      ])
+    }
+
+    return tasks
   }
 }
 </script>
